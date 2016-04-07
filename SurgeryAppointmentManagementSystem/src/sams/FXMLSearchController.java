518a485f9f5f4dd4d64f8bdb9f9465c587175bec
yurireis <yurireis@192.168.1.177>
2016-04-07 20:59:14 +0100

    Dynamic columns based on table search

--- SurgeryAppointmentManagementSystem/src/sams/FXMLSearchController.java
+++ SurgeryAppointmentManagementSystem/src/sams/FXMLSearchController.java
@@ -19,6 +19,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class FXMLSearchController implements Initializable {
@@ -49,15 +50,69 @@ public class FXMLSearchController implements Initializable {
             }
             switch(selectedTable){
                 case "Patients":
-                    columns.add(new TableColumn("Name"));
-                    columns.add(new TableColumn("Surname"));
-                    columns.add(new TableColumn("Mobile Phone"));
-                    columns.add(new TableColumn("Email"));
-                    columns.add(new TableColumn("Address"));
+                    TableColumn name = new TableColumn("Name");
+                    TableColumn surname = new TableColumn("Surname");
+                    TableColumn mobile = new TableColumn("Mobile Phone");
+                    //Maybe do a nested column to show the home phone too
+                    TableColumn email = new TableColumn("Email");
+                    TableColumn addr = new TableColumn("Address");
+                    
+                    columns.add(name);
+                    columns.add(surname);
+                    columns.add(mobile);
+                    columns.add(email);
+                    columns.add(addr);
+                    
+                    name.setCellValueFactory(new PropertyValueFactory<Patient, String>("pName"));
+                    surname.setCellValueFactory(new PropertyValueFactory<Patient, String>("pSurname"));
+                    mobile.setCellValueFactory(new PropertyValueFactory<Patient, String>("pMobPhone"));
+                    email.setCellValueFactory(new PropertyValueFactory<Patient, String>("pEmail"));
+                    addr.setCellValueFactory(new PropertyValueFactory<Patient, String>("pAddress"));
+                    
                     tableViewSearch.getColumns().addAll(columns);
+                    break;
                 case "Summaries":
+                    TableColumn spName = new TableColumn("Patient Name");
+                    TableColumn summary = new TableColumn("Summary");
+                    
+                    columns.add(spName);
+                    columns.add(summary);
+                    
+                    spName.setCellValueFactory(new PropertyValueFactory<Summary, String>("pName"));
+                    summary.setCellValueFactory(new PropertyValueFactory<Summary, String>("aSummary"));
+                    
+                    tableViewSearch.getColumns().addAll(columns);
+                    break;
                 case "Conditions":
-                case "Apointments":
+                    TableColumn cName = new TableColumn("Condition Name");
+                    TableColumn cDesc = new TableColumn("Description");
+                    TableColumn tLevel = new TableColumn("Threat level");
+                    
+                    columns.add(cName);
+                    columns.add(cDesc);
+                    columns.add(tLevel);
+                    
+                    cName.setCellValueFactory(new PropertyValueFactory<Condition, String>("cName"));
+                    cDesc.setCellValueFactory(new PropertyValueFactory<Condition, String>("cDesc"));
+                    tLevel.setCellValueFactory(new PropertyValueFactory<Condition, String>("cThreat_level"));
+                    
+                    tableViewSearch.getColumns().addAll(columns);
+                    break;
+                case "Appointment":
+                    TableColumn apName = new TableColumn("Patient Name");
+                    TableColumn aDate = new TableColumn("Date and time");
+                    TableColumn aType = new TableColumn("Type");
+                    
+                    columns.add(apName);
+                    columns.add(aDate);
+                    columns.add(aType);
+                    
+                    apName.setCellValueFactory(new PropertyValueFactory<Appointment, String>("pName"));
+                    aDate.setCellValueFactory(new PropertyValueFactory<Appointment, String>("aDatetime"));
+                    aType.setCellValueFactory(new PropertyValueFactory<Appointment, String>("aType"));
+                    
+                    tableViewSearch.getColumns().addAll(columns);
+                    break;
             }
             //do search with keyword string
             
